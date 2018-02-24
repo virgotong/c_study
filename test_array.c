6335e555ae45038f3c48bad4ffb68f56e256e3f8
root <root@zxsoft.(none)>
2018-02-24 16:48:16 +0800

    modify test_array

--- test_array.c
+++ test_array.c
@@ -13,7 +13,7 @@ void print_array(int *p1, int *p2)
 
 int main(int argc, char *argv[])
 {
-	int a[10] = {0,1,2,3,4,5,6,7,8,9};
+	int a[ 10 ] = {0,1,2,3,4,5,6,7,8,9};
 	print_array(&a[0], &a[8]);
 	return 0;
 }
